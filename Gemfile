7ec0d8c5608f77e8a8f1a60f6029c35d4913b81e
DinhDanhPhu <dinh.danh.phu@framgia.com>
2015-12-07 08:04:56 +0700

    Initialize repository

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'backbone-on-rails'
 gem 'rails_admin'
 
 group :development, :test do
-  gem 'sqlite3', '1.3.5'
+  gem 'sqlite3'
   gem 'rspec-rails', '2.11.0'
   gem 'guard-rspec', '1.2.1'
   gem 'guard-spork', '1.2.0'  
@@ -26,7 +26,6 @@ end
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails',   '3.2.5'
   gem 'coffee-rails', '3.2.2'
   gem 'uglifier', '1.2.3'
   gem 'handlebars_assets'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,17 +10,17 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require underscore
-//= require backbone
-//= require japanese
-//= require handlebars.runtime
-//= require handlebars
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
-//= require_tree .
+= require jquery
+= require jquery_ujs
+= require bootstrap
+= require underscore
+= require backbone
+= require japanese
+= require handlebars.runtime
+= require handlebars
+= require_tree ../templates
+= require_tree ./models
+= require_tree ./collections
+= require_tree ./views
+= require_tree ./routers
+= require_tree .
